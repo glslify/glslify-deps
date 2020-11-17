26ea3565f4228fb087dc9f7465bbf34a8ff35040
rubeniskov <me@rubeniskov.com>
2020-11-17 12:24:57 +0100

    simplify module exports

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
-var inherits = require('inherits')
 var NodeDepper = require('./node')
 
-/**
- * Creates a new instance of glslify-deps. Generally, you'll
- * want to use one instance per bundle.
- *
- * @class
- * @param {String|({
- *   cwd: String,
- *   transformRequire: Function
- * })} opts The root directory of your shader. Defaults to process.cwd()
- */
-function DepperAsync(opts) {
-  if (!(this instanceof DepperAsync)) return new DepperAsync(opts)
+module.exports = function(opts) {
   opts = (typeof opts === 'string' ? { cwd: opts } : opts) || {}
   opts.async = true
-  NodeDepper.call(this, opts);
+  return NodeDepper(opts)
 }
 
-inherits(DepperAsync, NodeDepper)
-module.exports = DepperAsync
+module.exports.sync = NodeDepper
--- sync.js
+++ sync.js
@@ -1,19 +1,3 @@
-var inherits = require('inherits')
 var NodeDepper = require('./node')
 
-/**
- * Creates a new instance of glslify-deps. Generally, you'll
- * want to use one instance per bundle.
- *
- * @class
- * @param {String|({
- *   cwd: String,
- * })} opts The root directory of your shader. Defaults to process.cwd()
- */
-function DepperSync(opts) {
-  if (!(this instanceof DepperSync)) return new DepperSync(opts)
-  NodeDepper.call(this, opts)
-}
-
-inherits(DepperSync, NodeDepper)
-module.exports = DepperSync
+module.exports = NodeDepper
