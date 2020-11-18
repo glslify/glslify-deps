d27201cd856153624dd94fe56fa0b9c105fdfd0a
rubeniskov <me@rubeniskov.com>
2020-11-18 13:03:07 +0100

    fix glslify tricky way to call applyTransforms

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ module.exports = (opts) => {
   return new NodeDepper(opts)
 }
 
+// glslify use applyTransforms in old fasion way...
+// https://github.com/glslify/glslify/blob/ba9c52c46e10068215290753e99ac516cc02d0d7/transform.js#L246
+module.exports.prototype.applyTransforms = NodeDepper.prototype.applyTransforms
+
 module.exports.sync = (opts) => {
   return new NodeDepper(opts)
 }
